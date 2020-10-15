2c4c7605c1e94edf6e4bf574dfa8e1715900ea8d
fj <castelloiv@gmail.com>
2020-10-15 14:16:22 -0400

    Added MongDB database and Mongoose, easy

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+router.post("/register", (req, res) => {
+  res.send("Yodal");
+});
+
+// router.post('/login', (req, res) => {
+
+// })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
-
+const authRoutes = require("./routes/authRoutes");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-console.log(process.env.MONGO);
+////Mongooooooose
+mongoose.connect(
+  process.env.MONGO,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("Connected to Mongoooooose")
+);
 
 const app = express();
+
+///Middlewaare for express routing
+app.use("/api/user", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
